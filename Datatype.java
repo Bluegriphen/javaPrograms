ddf7a93d2eb602664e09408f3c15d03b0caa4d78
Bluegriphen <156102865+Bluegriphen@users.noreply.github.com>
2024-01-18 11:20:47 -0800

    Datatype.java
    In this file define different datatypes in java code

---
+++ Datatype.java
@@ -0,0 +1,17 @@
+public class Datatype {
+    public static void main(String args[])
+    {
+        int a = 10;
+        byte b =4;
+        short s =56;
+        double d = 4.56456;
+        long c = 12121;
+        float f = 20.25f;
+        System.out.println("int:"+a);
+        System.out.println("byte:"+b);
+        System.out.println("short:"+s);
+        System.out.println("double:"+d);
+        System.out.println("long:"+c);
+        System.out.println("f="+f);
+    }
+}
